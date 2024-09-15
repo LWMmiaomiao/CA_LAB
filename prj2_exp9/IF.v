febe293df5f94639146938ec60e81e120cd60697
LWMmiaomiao <guoziying22@mails.ucas.ac.cn>
2024-09-15 14:31:52 +0800

    prj2 data hazard

---
+++ prj2_exp9/IF.v
@@ -0,0 +1,50 @@
+module if_stage(
+    input  wire        clk,
+    input  wire        reset,
+    input  wire        valid,
+    input  wire [31:0] inst_sram_rdata,
+    input  wire [32:0] br_signal,//{32：32}br_taken, {31：0}br_target
+    input  wire        ID_allowin,//暂时不考虑上一节所说的转移指令在译码阶段等待延迟槽指令取回，所以ID_allowin恒为1
+
+    output wire [ 3:0] inst_sram_we,
+    output wire        inst_sram_en,
+    output wire [31:0] inst_sram_addr,
+    output wire [31:0] inst_sram_wdata,
+    output wire        IF_readygo,
+    output wire        IF_allowin,
+    output wire        IDsignal_valid,//IF to ID signals : pc_ID
+    output wire [63:0] ID_signal
+);
+wire [31:0] nextpc;
+reg  [31:0] pc;
+wire [31:0] seq_pc;
+wire br_taken;
+wire [31:0] br_target;
+wire [31:0] inst;
+
+assign inst_sram_we    = 4'b0;
+assign inst_sram_en    = ID_allowin && !reset;
+assign inst_sram_addr  = ID_allowin ? nextpc : pc;//提前更新inst_sram_addr, 使得下一拍pc与指令同步更新
+assign inst_sram_wdata = 32'b0;
+assign inst            = inst_sram_rdata;
+
+assign {br_taken, br_target} = br_signal;
+assign seq_pc = pc + 3'h4;
+
+assign nextpc = br_taken ? br_target : seq_pc;
+
+always @(posedge clk) begin
+    if (reset) begin
+        pc <= 32'h1bfffffc;     //trick: to make nextpc be 0x1c000000 during reset 
+    end
+    else if (ID_allowin) begin// LOAD_DH时pc会被阻塞
+        pc <= nextpc;
+    end
+end
+
+assign IF_readygo = 1'b1;
+assign IF_allowin = IF_readygo && ID_allowin;// add in 9
+assign IDsignal_valid = valid && !br_taken;
+assign ID_signal ={inst, pc};
+
+endmodule
---
+++ prj2_exp9/WB.v
@@ -0,0 +1,38 @@
+module wb_stage(
+    input  wire        clk,
+    input  wire        reset,
+    input  wire        valid,
+    input  wire [69:0] signal,
+ 
+    output wire [31:0] debug_wb_pc,
+    output wire [ 3:0] debug_wb_rf_we,
+    output wire [ 4:0] debug_wb_rf_wnum,
+    output wire [31:0] debug_wb_rf_wdata,
+    output wire [ 4:0] rf_waddr,
+    output wire [31:0] rf_wdata,
+    output wire        rf_we,
+    output wire        WB_readygo,
+    output wire        WB_allowin
+);
+wire [31:0] pc;
+
+wire [31:0] final_result;
+
+wire [31:0] mem_result;
+
+wire        rf_we_WB;
+assign {pc, rf_we_WB, rf_waddr, final_result} = signal;
+assign rf_we = rf_we_WB && valid;
+
+
+
+assign rf_wdata = final_result;
+
+assign debug_wb_pc       = pc;
+assign debug_wb_rf_we    = {4{rf_we}};
+assign debug_wb_rf_wnum  = rf_waddr;
+assign debug_wb_rf_wdata = final_result;
+
+assign WB_readygo = 1'b1;
+assign WB_allowin = WB_readygo;
+endmodule
